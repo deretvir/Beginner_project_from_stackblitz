3a53d6e306c8e442469e54e972dc0855ad6e6524
deretvir <107854399+deretvir@users.noreply.github.com>
2023-07-19 18:21:12 +0200

    1 file modified

--- src/app/tests/test-paragraph/test-paragraph.component.ts
+++ src/app/tests/test-paragraph/test-paragraph.component.ts
@@ -2,10 +2,15 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-test-paragraph',
-  template:`tescik {{onStatus}}`,
+  template:`<p>property binding, value from Parent: {{onStatus}}</p>
+  <p [innerHtml]="propertyBindingFromClass"></p>
+  <input [(ngModel)]="twoWayDataBinding">
+  <p>Two way data binding (ngModel): {{twoWayDataBinding}} </p>
+  `,
   styleUrls: ['./test-paragraph.component.css']
 })
 export class TestParagraphComponent {
 @Input() onStatus:boolean;
-
+propertyBindingFromClass="propertyBindingFromClass";
+twoWayDataBinding:string='';
 }
